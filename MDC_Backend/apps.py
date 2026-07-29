dc8512413d1a4a99dae1a6c9544237fd610cc693
Manibalan <manibalan290@gmail.com>
2026-07-29 15:47:44 +0530

    Auto start notification scheduler on Django server startup via AppConfig.ready

--- MDC_Backend/apps.py
+++ MDC_Backend/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class MdcBackendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'MDC_Backend'
+
+    def ready(self):
+        try:
+            from .views import ensure_notification_scheduler
+            ensure_notification_scheduler()
+        except Exception as e:
+            print(f"Error starting notification scheduler in AppConfig: {e}")
+
